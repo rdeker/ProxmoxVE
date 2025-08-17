f97697868cda49309e2f5948551ac79961407b21
deker <deker@deker.co>
2025-08-17 18:16:25 -0400

    change tdarr UID/GID

--- install/tdarr-deker-install.sh
+++ install/tdarr-deker-install.sh
@@ -29,7 +29,9 @@ fi
 msg_ok "Set Up Hardware Acceleration"
 
 msg_info "Adding tdarr user"
-$STD useradd -d /opt/tdarr tdarr
+$STD useradd -d /opt/tdarr -g 999 -u 999 tdarr
+$STD usermod -aG video tdarr
+$STD usermod -aG render tdarr
 
 msg_info "Installing Tdarr"
 mkdir -p /opt/tdarr
